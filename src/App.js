f1af21329f214bb4cc310b0e9dbd180493fc2198
Tasmia-Ishika <ishika.tasmia3787@gmail.com>
2022-03-26 14:39:33 +0600

    fifth commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,18 @@ import Shop from './components/Shop/Shop';
 function App() {
   return (
     <div className="App">
-     <Navbar></Navbar>
-     <Shop></Shop>
+      <Navbar></Navbar>
+      <Shop></Shop>
+      <br />
+      <div>
+        <h1 className='writings'>How React works?</h1>
+        <p>ANSWER : React is a JavaScript library that creates user interfaces (UIs) in a effective and predictable way by using declarative code. We can use it to build single page applications and mobile apps, or to build complex apps if we utilise it with other libraries.
+          React basically maintains like a tree. This tree can do efficient computations on the nodes.If I think HTML code as a tree,that is exactly how the browser treats DOM . React allows to re-construct effectively any DOM in JavaScript and push only those changes to the DOM which have actually occurred.
+        </p>
+      </div>
     </div>
   );
 }
+
 
 export default App;
